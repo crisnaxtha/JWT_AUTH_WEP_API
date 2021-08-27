546b98e74b0da85a2af28668133eefb5c3034282
Krishna Shrestha <krishnashrestha49@gmail.com>
2021-08-28 00:52:40 +0545

    Add Migration CreateUsersTable and UserContext

---
+++ MeroKhata/Data/UserContext.cs
@@ -0,0 +1,24 @@
+﻿using MeroKhata.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeroKhata.Data
+{
+    public class UserContext: DbContext 
+    {
+        public UserContext(DbContextOptions<UserContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { set; get; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>(entity => { entity.HasIndex(e => e.Email).IsUnique(); });
+        }
+    }
+}
